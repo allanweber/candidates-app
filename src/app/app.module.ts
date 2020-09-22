f776e73a1b986d0d6a88d2088e8d6aa8e1b3424b
Allan Weber <a.cassianoweber@gmail.com>
2020-09-22 18:11:54 +0200

    Fix burger menu toogle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { CandidatesModule } from './candidates/candidates.module';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,13 +6,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
-import { SharedModule } from './shared/shared.module';
+import { CandidatesModule } from './candidates/candidates.module';
 import { AuthenticationInterceptorService } from './core/interceptors/authentication-interceptor.service';
 import { HttpErrorInterceptor } from './core/interceptors/http-error-interceptor';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
