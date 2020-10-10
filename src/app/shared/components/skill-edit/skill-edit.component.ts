ac414823a090a4d035b8792136a0b709a9794c88
Allan Weber <a.cassianoweber@gmail.com>
2020-10-10 16:57:54 +0200

    Complete component with options

--- src/app/shared/components/skill-edit/skill-edit.component.ts
+++ src/app/shared/components/skill-edit/skill-edit.component.ts
@@ -7,36 +7,83 @@ import { Skill } from './../../model/skill.model';
   styleUrls: ['./skill-edit.component.scss'],
 })
 export class SkillEditComponent implements OnInit {
-  @Input() skill: Skill;
+  @Input() skills: Skill[];
   @Input() editable = true;
+  @Input() removable = true;
+  @Input() yearsVisible = true;
+  @Input() addable = true;
+  @Input() tagSize = 'are-medium';
 
-  @Output() removed: EventEmitter<Skill> = new EventEmitter<Skill>();
-  @Output() changed: EventEmitter<Skill> = new EventEmitter<Skill>();
+  @Output() changed: EventEmitter<Skill[]> = new EventEmitter<Skill[]>();
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  getYearsText(): string {
-    return `${this.skill.years} ano${this.skill.years === 1 ? '' : 's'}`;
-  }
-
-  change(year: number): void {
-    this.skill.years += year;
-    if (this.skill.years < 0) {
-      this.skill.years = 0;
+  addSkill(skillInput: HTMLInputElement): void {
+    const skillName = skillInput.value;
+    if (!skillName || skillName.trim() === '') {
+      return;
     }
-    if (this.skill.years > 99) {
-      this.skill.years = 99;
+
+    if (!this.skills) {
+      this.skills = [];
     }
-    this.changed.emit(this.skill);
+
+    let skillValues = skillName.split(/,|;/).map((x) => x.trim());
+    skillValues = skillValues.filter(
+      (val) => !this.skills.map((skill) => skill.name).includes(val)
+    );
+
+    skillValues.forEach((skill) => this.skills.push({ name: skill, years: 0 }));
+
+    this.changed.emit(this.skills);
+    skillInput.value = '';
+    skillInput.focus();
   }
 
-  remove(): void {
-    this.removed.emit(this.skill);
+  getYearsText(skill: Skill): string {
+    return `- ${skill.years > 0 && skill.years < 10 ? '0' : ''}${
+      skill.years
+    } ano${skill.years === 1 ? '' : 's'}`;
   }
 
-  isEditable(): boolean {
-    return this.editable === true;
+  change(skill: Skill, year: number): void {
+    skill.years += year;
+    if (skill.years < 0) {
+      skill.years = 0;
+    }
+    if (skill.years > 99) {
+      skill.years = 99;
+    }
+    const skillIndex = this.skills.indexOf(skill);
+    if (skillIndex > -1) {
+      this.skills[skillIndex] = skill;
+    }
+    this.changed.emit(this.skills);
+  }
+
+  remove(skill: Skill): void {
+    const skillIndex = this.skills.indexOf(skill);
+    if (skillIndex > -1) {
+      this.skills.splice(skillIndex, 1);
+    }
+    this.changed.emit(this.skills);
+  }
+
+  get isEditable(): boolean {
+    return this.editable === true && this.showYears;
+  }
+
+  get isRemovable(): boolean {
+    return this.removable === true;
+  }
+
+  get showYears(): boolean {
+    return this.yearsVisible === true;
+  }
+
+  get isAddable(): boolean {
+    return this.addable === true;
   }
 }
